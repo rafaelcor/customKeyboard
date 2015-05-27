44f8b6503012c8e82d34ccac0a9dd9379ee751c4
Rafael Cordano <rafael.cordano@gmail.com>
2015-05-27 20:06:33 -0300

    Movment needs fixes

--- ck.py
+++ ck.py
@@ -21,14 +21,21 @@ import gtk
 
 
 class CustomKey:
+    def move_key(self, widget, event, key):
+        self.evpos = self.save[key][0:2]
+        self.evpos[0] += int(event.x - 25)
+        self.evpos[1] += int(event.y - 25)
+        self.fixed.move(widget, self.evpos[0], self.evpos[1])
 
     def change_name(self, widget, event, key, button):
         print event.keyval
 
         if event.keyval == 65293 or event.keyval == 65421:
             self.save[key][4] = widget.get_text()
-            button.set_label(widget.get_text())
             widget.destroy()
+            button.add(gtk.Label(self.save[key][4]))
+            button.show_all()
+
 
     def onButtonDoubleClick(self, widget, event, key):
         print ("2 clicked")
@@ -45,28 +52,26 @@ class CustomKey:
         widget.show_all()
 
     def new_button(self, widget, event):
-        #print (event.keyval)
+        #print "new"
         if event.keyval == 110:
-            #print("110")
             x, y = self.save[self.save.keys()[-1]][0:2]
             self.save[self.cont] = [x + 50, y, 50, 50, ""]
 
-            b = gtk.Button()
+            b = gtk.EventBox()
+            b.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(red=65535, green=65535, blue=65535))
+
             b.set_size_request(self.save[self.cont][2], self.save[self.cont][3])
-            self.eventbox.connect('button-press-event', self.onButtonPress, b, self.cont)
-            self.eventbox.connect('motion-notify-event', self.onButtonPress, b, self.cont)
-            #b.connect("motion-notify-event", self.mne)
-            #b.connect("button-press-event", self.onButtonPress, self.cont)
-            #b.connect("button-release-event", self.onButtonRelease, self.cont)
+            b.connect('button-press-event', self.move_key, self.cont)
+            b.connect('motion-notify-event', self.move_key, self.cont)
             self.fixed.put(b, self.save[self.cont][0], self.save[self.cont][1])
             self.window.show_all()
 
         self.cont += 1
 
-    def onButtonPress(self, widget, event, widget2, key):
-        
+    def onButtonPress(self, widget, event, key):
+
         if event.type == gtk.gdk._2BUTTON_PRESS:
-            self.onButtonDoubleClick(widget2, event, key)
+            self.onButtonDoubleClick(widget, event, key)
 
         else:
             self.evpos = self.save[key][0:2]
@@ -105,24 +110,27 @@ class CustomKey:
             gtk.gdk.BUTTON3_MOTION_MASK
         )
         self.fixed = gtk.Fixed()
-        #self.hbox = gtk.HBox()
 
         for key in self.save:
-            self.b = gtk.EventBox()
-            self.b.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(red=65535, green=65535, blue=65535))
+            self.bb = gtk.EventBox()
+            self.bb.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(red=65535, green=65535, blue=65535))
             #self.bq.set_size_request(50, 50)
-            self.b.set_size_request(self.save[key][2], self.save[key][3])
+            self.bb.set_size_request(self.save[key][2], self.save[key][3])
             #self.b.connect("button-press-event", self.onButtonPress, key)
             #self.b.connect("button-release-event", self.onButtonRelease, key)
-            self.eventbox.connect('button-press-event', self.onButtonPress, self.b, key)
-            self.eventbox.connect('motion-notify-event', self.onButtonPress, self.b, key)
-            self.fixed.put(self.b, self.save[key][0], self.save[key][1])
+            #self.eventbox.connect('button-press-event', self.onButtonPress, self.b, key)
+            #self.eventbox.connect('motion-notify-event', self.onButtonPress, self.b, key)
+
+            self.bb.connect('button-press-event', self.onButtonPress, key)
+            self.bb.connect('motion-notify-event', self.move_key, key)
+            self.fixed.put(self.bb, self.save[key][0], self.save[key][1])
 
         window.add_events(gtk.gdk.MOTION_NOTIFY | gtk.gdk.BUTTON_PRESS)
 
-        #window.connect("button-press-event", self.onWindowPressEvent)
         window.connect("key-press-event", self.new_button)
         window.connect("destroy", gtk.main_quit)
+
+
 
         window.add(self.eventbox)
         self.eventbox.add(self.fixed)

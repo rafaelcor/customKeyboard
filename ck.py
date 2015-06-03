6bc8efacd43944922bef00a66ff72045b812554c
Rafael Cordano <rafael.cordano@gmail.com>
2015-06-02 22:50:22 -0300

    started with the port to gtk3

--- ck.py
+++ ck.py
@@ -16,14 +16,43 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 import json
-import gtk
+
+from gi.repository import Gtk, Gdk
 #import gobject
 
 INITIAL_X, INITIAL_Y = 0, 50
 
+UI_INFO = """
+<ui>
+  <menubar name='MenuBar'>
+    <menu action='FileMenu'>
+      <menuitem action='FileNewStandard' />
+      <menuitem action='FileOpen' />
+      <menuitem action='FileSave' />
+      <menuitem action='FileSaveAs' />
+      <separator />
+      <menuitem action='FileQuit' />
+    </menu>
+    <menu action='OptionMenu'>
+      <menuitem action='OptionRunKeyboard' />
+    </menu>
+    <menu action='HelpMenu'>
+      <menuitem action='Help'/>
+      <menuitem action='About'/>
+    </menu>
+  </menubar>
+  <toolbar name='ToolBar'>
+    <toolitem action='FileNewStandard' />
+    <toolitem action='FileOpen' />
+    <toolitem action='FileQuit' />
+  </toolbar>
+</ui>
+"""
+
 
 class CustomKey:
     def __init__(self):
+        """
         self.menu_items = (
             ("/_File",         None,         None, 0, "<Branch>"),
             ("/File/_New",     "<control>N", None, 0, None),
@@ -31,65 +60,76 @@ class CustomKey:
             ("/File/_Save",    "<control>S", None, 0, None ),
             ("/File/Save _As", None,         self.saveToJSON, 0, None ),
             ("/File/sep1",     None,         None, 0, "<Separator>" ),
-            ("/File/Quit",     "<control>Q", gtk.main_quit, 0, None ),
+            ("/File/Quit",     "<control>Q", Gtk.main_quit, 0, None ),
             ("/_Options",      None,         None, 0, "<Branch>" ),
             ("/Options/Run keyboard",  "<control>R", self.run , 0, None ),
             ("/_Help",         None,         None, 0, "<LastBranch>" ),
             ("/_Help/About",   None,         None, 0, None ),
             )
+        """
+        ####Main Menu
+        action_group = Gtk.ActionGroup("my_actions")
+        self.add_file_menu_actions(action_group)
+        self.add_options_menu_actions(action_group)
+
+
         self.mousePosition = []
         self.cont = 1
         self.save = {}  # "button name":[x, y, sx, sy , label, func]
         self.edit = 0
         self.evpos = []
-        window = gtk.Window()
+        window = Gtk.Window()
         self.window = window
         window.set_size_request(window.get_screen().get_width(),
                                 window.get_screen().get_height() / 3)
-        self.eventbox = gtk.EventBox()
+        self.eventbox = Gtk.EventBox()
         self.eventbox.set_events(
-            gtk.gdk.BUTTON_MOTION_MASK |               # restoring missed masks
-            gtk.gdk.BUTTON1_MOTION_MASK |
-            gtk.gdk.BUTTON2_MOTION_MASK |
-            gtk.gdk.BUTTON3_MOTION_MASK
+            Gdk.EventMask.BUTTON_MOTION_MASK |               # restoring missed masks
+            Gdk.EventMask.BUTTON1_MOTION_MASK |
+            Gdk.EventMask.BUTTON2_MOTION_MASK |
+            Gdk.EventMask.BUTTON3_MOTION_MASK
         )
-        self.fixed = gtk.Fixed()
+        self.fixed = Gtk.Fixed()
         self.fixed.set_size_request(window.get_screen().get_width(),
                                     window.get_screen().get_height() / 3
                                     )
 
-        self.menu = gtk.Menu()
-        self.menu_optionEditProperties = gtk.MenuItem("Edit Properties")
-        self.menu_deleteButton = gtk.MenuItem("Remove Button")
+        self.menu = Gtk.Menu()
+        self.menu_optionEditProperties = Gtk.MenuItem("Edit Properties")
+        self.menu_deleteButton = Gtk.MenuItem("Remove Button")
         self.menu.add(self.menu_optionEditProperties)
         self.menu.add(self.menu_deleteButton)
         self.menu.show_all()
 
         self.init()
-        window.add_events(gtk.gdk.MOTION_NOTIFY | gtk.gdk.BUTTON_PRESS)
+        window.add_events(Gdk.EventMask.POINTER_MOTION_MASK |
+                          Gdk.EventMask.BUTTON_PRESS_MASK)
 
         window.connect("key-press-event", self.new_button)
-        window.connect("destroy", gtk.main_quit)
+        window.connect("destroy", Gtk.main_quit)
 
-        self.vbox = gtk.VBox(False, 1)
+        self.vbox = Gtk.VBox(False, 1)
         self.vbox.set_border_width(1)
 
         window.add(self.vbox)
-        menubar = self.get_main_menu(window)
+        uimanager = self.create_ui_manager()
+        uimanager.insert_action_group(action_group)
+
+        menubar = uimanager.get_widget("/MenuBar")
         self.vbox.pack_start(menubar, False, True, 0)
         self.vbox.add(self.eventbox)
         self.eventbox.add(self.fixed)
         window.show_all()
 
     def run(self, widget, event):
-        self.window2 = gtk.Window()
-        self.fixed2 = gtk.Fixed()
+        self.window2 = Gtk.Window()
+        self.fixed2 = Gtk.Fixed()
         self.window2.add(self.fixed2)
         self.window2.show_all()
         self.fixed2.show_all()
 
         for key in self.save:
-            button = gtk.Button(self.save[key][4])
+            button = Gtk.Button(self.save[key][4])
             button.set_size_request(self.save[key][2], self.save[key][3])
             self.fixed2.put(button, self.save[key][0], self.save[key][1])
             button.show()
@@ -99,9 +139,9 @@ class CustomKey:
         for child in self.fixed.get_children():
             child.destroy()
         for key in self.save:
-            self.bb = gtk.EventBox()
+            self.bb = Gtk.EventBox()
             self.bb.set_border_width(1)
-            self.bb.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(red=65535,
+            self.bb.modify_bg(Gtk.StateFlags.NORMAL, Gdk.Color(red=65535,
                                                               green=65535,
                                                               blue=65535))
             self.bb.set_size_request(self.save[key][2], self.save[key][3])
@@ -118,7 +158,7 @@ class CustomKey:
                                                    self.editButton,
                                                    self.cont,
                                                    self.bb)
-            self.bb.add(gtk.Label(self.save[key][4]))
+            self.bb.add(Gtk.Label(self.save[key][4]))
             self.fixed.put(self.bb, self.save[key][0], self.save[key][1])
 
     def test(self, widget, event):
@@ -126,16 +166,16 @@ class CustomKey:
         #if event.x <= 25:
          #   widget.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.SIZING))
 
-    def openFromJSON(self, widget, data):
-        fileChooserDialog = gtk.FileChooserDialog("Save Keyboard", None,
-         gtk.FILE_CHOOSER_ACTION_OPEN, (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-         gtk.STOCK_OK, gtk.RESPONSE_OK))
-        keyboardFilter = gtk.FileFilter()
+    def openFromJSON(self, data):
+        fileChooserDialog = Gtk.FileChooserDialog("Save Keyboard", None,
+         Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+         Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        keyboardFilter = Gtk.FileFilter()
         keyboardFilter.set_name("Keyboard Files")
         keyboardFilter.add_pattern("*.keyboard")
         fileChooserDialog.add_filter(keyboardFilter)
         response = fileChooserDialog.run()
-        if response == gtk.RESPONSE_OK:
+        if response == Gtk.ResponseType.OK:
             fileName = fileChooserDialog.get_filename()
             print fileName
             f = open(fileName, "r")
@@ -143,16 +183,16 @@ class CustomKey:
             self.save = json.loads(fr[0])
             self.init()
             self.window.show_all()
-        elif response == gtk.RESPONSE_CANCEL:
+        elif response == Gtk.RESPONSE_CANCEL:
             pass
         fileChooserDialog.destroy()
 
     def saveToJSON(self, widget, data):
-        fileChooserDialog = gtk.FileChooserDialog("Save Keyboard", None,
-         gtk.FILE_CHOOSER_ACTION_SAVE, (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-         gtk.STOCK_OK, gtk.RESPONSE_OK))
+        fileChooserDialog = Gtk.FileChooserDialog("Save Keyboard", None,
+         Gtk.FILE_CHOOSER_ACTION_SAVE, (Gtk.STOCK_CANCEL, Gtk.RESPONSE_CANCEL,
+         Gtk.STOCK_OK, Gtk.RESPONSE_OK))
         response = fileChooserDialog.run()
-        if response == gtk.RESPONSE_OK:
+        if response == Gtk.RESPONSE_OK:
             fileName = fileChooserDialog.get_filename()
             print fileName
             if not fileName.endswith('.keyboard'):
@@ -160,15 +200,58 @@ class CustomKey:
             f = open(fileName, "w")
             f.write(json.dumps(self.save))
 
-        elif response == gtk.RESPONSE_CANCEL:
+        elif response == Gtk.RESPONSE_CANCEL:
             pass
         fileChooserDialog.destroy()
 
         pass
 
+    def add_file_menu_actions(self, action_group):
+        action_filemenu = Gtk.Action("FileMenu", "File", None, None)
+        action_group.add_action(action_filemenu)
+
+        action_filenew = Gtk.Action("FileNewStandard", None, None, Gtk.STOCK_NEW)
+        action_group.add_action(action_filenew)
+
+        action_fileopen = Gtk.Action("FileOpen", None, None, Gtk.STOCK_OPEN)
+        action_group.add_action(action_fileopen)
+        action_fileopen.connect("activate", self.openFromJSON)
+
+        action_filesave = Gtk.Action("FileSave", None, None, Gtk.STOCK_SAVE)
+        action_group.add_action(action_filesave)
+
+        action_filesaveas = Gtk.Action("FileSaveAs", None, None, Gtk.STOCK_SAVE_AS)
+        action_group.add_action(action_filesaveas)
+        action_filesaveas.connect("activate", self.saveToJSON)
+
+        action_filequit = Gtk.Action("FileQuit", None, None, Gtk.STOCK_QUIT)
+        action_group.add_action(action_filequit)
+
+
+
+
+    def add_options_menu_actions(self, action_group):
+        action_optionmenu = Gtk.Action("OptionMenu", "Options", None, None)
+        action_group.add_action(action_optionmenu)
+
+        action_optionrunkeyboard = Gtk.Action("OptionRunKeyboard", None, None, Gtk.STOCK_EXECUTE)
+        action_group.add_action(action_optionrunkeyboard)
+
+
+    def create_ui_manager(self):
+        uimanager = Gtk.UIManager()
+
+        # Throws exception if something went wrong
+        uimanager.add_ui_from_string(UI_INFO)
+
+        # Add the accelerator group to the toplevel window
+        accelgroup = uimanager.get_accel_group()
+        self.window.add_accel_group(accelgroup)
+        return uimanager
+
     def get_main_menu(self, window):
-        accel_group = gtk.AccelGroup()
-        item_factory = gtk.ItemFactory(gtk.MenuBar, "<main>", accel_group)
+        accel_group = Gtk.AccelGroup()
+        item_factory = Gtk.ItemFactory(Gtk.MenuBar, "<main>", accel_group)
 
         item_factory.create_items(self.menu_items)
 
@@ -190,14 +273,14 @@ class CustomKey:
         if event.keyval == 65293 or event.keyval == 65421:
             self.save[key][4] = widget.get_text()
             widget.destroy()
-            button.add(gtk.Label(self.save[key][4]))
+            button.add(Gtk.Label(self.save[key][4]))
             button.show_all()
 
     def onButtonDoubleClick(self, widget, event, key):
         print ("2 clicked")
         if widget.get_child() is not None:
             widget.remove(widget.get_child())
-        entry = gtk.Entry()
+        entry = Gtk.Entry()
         if self.save[key][4] != "":
             entry.set_text(self.save[key][4])
         entry.set_can_focus(1)
@@ -216,8 +299,8 @@ class CustomKey:
                 x, y = INITIAL_X, INITIAL_Y
             self.save[self.cont] = [x + 50, y, 50, 50, ""]
 
-            b = gtk.EventBox()
-            b.modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(red=65535,
+            b = Gtk.EventBox()
+            b.modify_bg(Gtk.StateFlags.NORMAL, Gdk.Color(red=65535,
                                                         green=65535,
                                                         blue=65535))
             b.set_border_width(1)
@@ -240,7 +323,7 @@ class CustomKey:
 
     def onButtonPress(self, widget, event, key):
 
-        if event.type == gtk.gdk._2BUTTON_PRESS:
+        if event.type == Gdk._2BUTTON_PRESS:
             self.onButtonDoubleClick(widget, event, key)
 
         else:
@@ -262,7 +345,7 @@ class CustomKey:
             pass
 
     def onButtonRightClick(self, widget, event):
-        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
+        if event.type == Gdk.BUTTON_PRESS and event.button == 3:
             widget.popup(None, None, None, event.button, event.time)
             print
             pass
@@ -278,4 +361,4 @@ class CustomKey:
 
 if __name__ == "__main__":
     CustomKey()
-    gtk.main()
+    Gtk.main()
